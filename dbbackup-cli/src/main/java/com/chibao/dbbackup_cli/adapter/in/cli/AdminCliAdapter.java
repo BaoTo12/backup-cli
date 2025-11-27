002417fb9a824203e1e204e5e04ae371941ab36f
ChiBao <baotochi87@gmail.com>
2025-11-27 13:30:00 +0700

    update: remove unused modules

--- dbbackup-cli/src/main/java/com/chibao/dbbackup_cli/adapter/in/cli/AdminCliAdapter.java
+++ dbbackup-cli/src/main/java/com/chibao/dbbackup_cli/adapter/in/cli/AdminCliAdapter.java
@@ -2,10 +2,22 @@ package com.chibao.dbbackup_cli.adapter.in.cli;
 
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 
 @ShellComponent
 class AdminCliAdapter {
 
+//
+//    @ShellMethod(key = "test-connection", value = "Test database connection")
+//    public String testConnection(
+//            @ShellOption(help = "Database type (mysql, postgres, sqlite, mongodb)") String dbtype,
+//            @ShellOption(help = "Database host", defaultValue = "localhost") String host,
+//            @ShellOption(help = "Database port", defaultValue = "") String port,
+//            @ShellOption(help = "Database username", defaultValue = "") String username,
+//            @ShellOption(help = "Database password", defaultValue = "") String password,
+//            @ShellOption(help = "Database name") String dbname) {
+//        return databaseConnectionService.testConnection(dbtype, host, port, username, password, dbname);
+//    }
     @ShellMethod(value = "Display version", key = "version")
     public String version() {
         return "DB Backup CLI v1.0.0";
